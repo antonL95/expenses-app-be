e5234e6a56b315c3a2b0be76e9a5d2b0492915b7
Anton Loginov <loginovanton95@gmail.com>
2023-12-19 09:20:29 +0100

    Add GetCurrentBankAccountBalanceCommand
    This commit introduces a new console command to retrieve the current bank account balance. It handles the request to the bank API, processes the returned data, and updates the accountBalance field in the UserBankAccounts model.

---
+++ app/Console/Commands/GetCurrentBankAccountBalanceCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Models\UserBankAccounts;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+
+class GetCurrentBankAccountBalanceCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'app:get-current-bank-account-balance-command';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Get current account balance';
+
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $to = Carbon::now()
+            ->setTime(0, 0);
+
+        $url = Config::get('app.fio.transactionsUrl');
+        if (!\is_string($url)) {
+            throw new \InvalidArgumentException('Invalid URL');
+        }
+
+        $bankAccounts = UserBankAccounts::all();
+
+        foreach ($bankAccounts as $bankAccount) {
+            $url = sprintf(
+                $url,
+                $bankAccount->accountApiToken,
+                $to->format('Y-m-d'),
+                $to->format('Y-m-d'),
+            );
+
+            $json = Http::get($url)->json();
+
+            if (!\is_array($json)) {
+                return 255;
+            }
+
+            if (!isset($json['accountStatement']['info']['closingBalance'])) {
+                continue;
+            }
+
+            $bankAccount->accountBalance = (float) $json['accountStatement']['info']['closingBalance'];
+            $bankAccount->save();
+        }
+
+        return 0;
+    }
+}
